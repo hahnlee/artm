a9178d3742cbb3c75b567e228e4dff0c591f434b
hahnlee <hanlee.dev@gmail.com>
2026-08-15 16:41:45 +0900

    feat(libcore): bridge Character natives to ICU

--- darwin-art/compat/darwin_libcore_natives.cc
+++ darwin-art/compat/darwin_libcore_natives.cc
@@ -15,9 +15,12 @@
 #include <bit>
 #include <cerrno>
 #include <chrono>
+#include <cmath>
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
 #include <cstdlib>
+#include <iterator>
 #include <string>
 #include <vector>
 
@@ -37,6 +40,113 @@ jlong DoubleToRawLongBits(JNIEnv*, jclass, jdouble value) {
 
 jdouble LongBitsToDouble(JNIEnv*, jclass, jlong bits) {
   return std::bit_cast<double>(static_cast<std::int64_t>(bits));
+}
+
+jint CharacterDigit(JNIEnv*, jclass, jint code_point, jint radix) {
+  return u_digit(code_point, radix);
+}
+
+jbyte CharacterDirectionality(JNIEnv*, jclass, jint code_point) {
+  return static_cast<jbyte>(u_charDirection(code_point));
+}
+
+jstring CharacterName(JNIEnv* env, jclass, jint code_point) {
+  const bool is_control =
+      code_point <= 0x1f || (code_point >= 0x7f && code_point <= 0x9f);
+  const UCharNameChoice choice =
+      is_control ? U_EXTENDED_CHAR_NAME : U_UNICODE_CHAR_NAME;
+  char name[BUFSIZ];
+  UErrorCode status = U_ZERO_ERROR;
+  const std::int32_t length =
+      u_charName(code_point, choice, name, sizeof(name), &status);
+  return U_FAILURE(status) || length == 0 ? nullptr : env->NewStringUTF(name);
+}
+
+jint CharacterNumericValue(JNIEnv*, jclass, jint code_point) {
+  const double result = u_getNumericValue(code_point);
+  if (result == U_NO_NUMERIC_VALUE) {
+    return -1;
+  }
+  if (result < 0 || std::floor(result + 0.5) != result) {
+    return -2;
+  }
+  return static_cast<jint>(result);
+}
+
+jint CharacterType(JNIEnv*, jclass, jint code_point) {
+  return u_charType(code_point);
+}
+
+jboolean CharacterIsAlphabetic(JNIEnv*, jclass, jint code_point) {
+  return u_hasBinaryProperty(code_point, UCHAR_ALPHABETIC);
+}
+
+jboolean CharacterIsDefined(JNIEnv*, jclass, jint code_point) {
+  return u_isdefined(code_point);
+}
+
+jboolean CharacterIsDigit(JNIEnv*, jclass, jint code_point) {
+  return u_isdigit(code_point);
+}
+
+jboolean CharacterIsIdentifierIgnorable(JNIEnv*, jclass, jint code_point) {
+  return u_isIDIgnorable(code_point);
+}
+
+jboolean CharacterIsIdeographic(JNIEnv*, jclass, jint code_point) {
+  return u_hasBinaryProperty(code_point, UCHAR_IDEOGRAPHIC);
+}
+
+jboolean CharacterIsLetter(JNIEnv*, jclass, jint code_point) {
+  return u_isalpha(code_point);
+}
+
+jboolean CharacterIsLetterOrDigit(JNIEnv*, jclass, jint code_point) {
+  return u_isalnum(code_point);
+}
+
+jboolean CharacterIsLowerCase(JNIEnv*, jclass, jint code_point) {
+  return u_islower(code_point);
+}
+
+jboolean CharacterIsMirrored(JNIEnv*, jclass, jint code_point) {
+  return u_isMirrored(code_point);
+}
+
+jboolean CharacterIsSpaceChar(JNIEnv*, jclass, jint code_point) {
+  return u_isJavaSpaceChar(code_point);
+}
+
+jboolean CharacterIsTitleCase(JNIEnv*, jclass, jint code_point) {
+  return u_istitle(code_point);
+}
+
+jboolean CharacterIsUnicodeIdentifierPart(JNIEnv*, jclass, jint code_point) {
+  return u_isIDPart(code_point);
+}
+
+jboolean CharacterIsUnicodeIdentifierStart(JNIEnv*, jclass, jint code_point) {
+  return u_isIDStart(code_point);
+}
+
+jboolean CharacterIsUpperCase(JNIEnv*, jclass, jint code_point) {
+  return u_isupper(code_point);
+}
+
+jboolean CharacterIsWhitespace(JNIEnv*, jclass, jint code_point) {
+  return u_isWhitespace(code_point);
+}
+
+jint CharacterToLowerCase(JNIEnv*, jclass, jint code_point) {
+  return u_tolower(code_point);
+}
+
+jint CharacterToTitleCase(JNIEnv*, jclass, jint code_point) {
+  return u_totitle(code_point);
+}
+
+jint CharacterToUpperCase(JNIEnv*, jclass, jint code_point) {
+  return u_toupper(code_point);
 }
 
 void OsConstantsInitConstants(JNIEnv* env, jclass klass) {
@@ -303,6 +413,58 @@ bool Register(JNIEnv* env, const char* class_name,
 namespace darwin_art {
 
 bool RegisterLibcoreNatives(JNIEnv* env) {
+  JNINativeMethod character_methods[] = {
+      {const_cast<char*>("digitImpl"), const_cast<char*>("(II)I"),
+       reinterpret_cast<void*>(&CharacterDigit)},
+      {const_cast<char*>("getDirectionalityImpl"), const_cast<char*>("(I)B"),
+       reinterpret_cast<void*>(&CharacterDirectionality)},
+      {const_cast<char*>("getNameImpl"),
+       const_cast<char*>("(I)Ljava/lang/String;"),
+       reinterpret_cast<void*>(&CharacterName)},
+      {const_cast<char*>("getNumericValueImpl"), const_cast<char*>("(I)I"),
+       reinterpret_cast<void*>(&CharacterNumericValue)},
+      {const_cast<char*>("getTypeImpl"), const_cast<char*>("(I)I"),
+       reinterpret_cast<void*>(&CharacterType)},
+      {const_cast<char*>("isAlphabeticImpl"), const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsAlphabetic)},
+      {const_cast<char*>("isDefinedImpl"), const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsDefined)},
+      {const_cast<char*>("isDigitImpl"), const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsDigit)},
+      {const_cast<char*>("isIdentifierIgnorableImpl"),
+       const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsIdentifierIgnorable)},
+      {const_cast<char*>("isIdeographicImpl"), const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsIdeographic)},
+      {const_cast<char*>("isLetterImpl"), const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsLetter)},
+      {const_cast<char*>("isLetterOrDigitImpl"), const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsLetterOrDigit)},
+      {const_cast<char*>("isLowerCaseImpl"), const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsLowerCase)},
+      {const_cast<char*>("isMirroredImpl"), const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsMirrored)},
+      {const_cast<char*>("isSpaceCharImpl"), const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsSpaceChar)},
+      {const_cast<char*>("isTitleCaseImpl"), const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsTitleCase)},
+      {const_cast<char*>("isUnicodeIdentifierPartImpl"),
+       const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsUnicodeIdentifierPart)},
+      {const_cast<char*>("isUnicodeIdentifierStartImpl"),
+       const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsUnicodeIdentifierStart)},
+      {const_cast<char*>("isUpperCaseImpl"), const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsUpperCase)},
+      {const_cast<char*>("isWhitespaceImpl"), const_cast<char*>("(I)Z"),
+       reinterpret_cast<void*>(&CharacterIsWhitespace)},
+      {const_cast<char*>("toLowerCaseImpl"), const_cast<char*>("(I)I"),
+       reinterpret_cast<void*>(&CharacterToLowerCase)},
+      {const_cast<char*>("toTitleCaseImpl"), const_cast<char*>("(I)I"),
+       reinterpret_cast<void*>(&CharacterToTitleCase)},
+      {const_cast<char*>("toUpperCaseImpl"), const_cast<char*>("(I)I"),
+       reinterpret_cast<void*>(&CharacterToUpperCase)},
+  };
   JNINativeMethod float_methods[] = {
       {const_cast<char*>("floatToRawIntBits"), const_cast<char*>("(F)I"),
        reinterpret_cast<void*>(&FloatToRawIntBits)},
@@ -365,7 +527,9 @@ bool RegisterLibcoreNatives(JNIEnv* env) {
       {const_cast<char*>("isSocket"), const_cast<char*>("(I)Z"),
        reinterpret_cast<void*>(&FileDescriptorIsSocket)},
   };
-  return Register(env, "java/lang/Float", float_methods, 2) &&
+  return Register(env, "java/lang/Character", character_methods,
+                  static_cast<jint>(std::size(character_methods))) &&
+         Register(env, "java/lang/Float", float_methods, 2) &&
          Register(env, "java/lang/Double", double_methods, 2) &&
          Register(env, "android/system/OsConstants", os_constants_methods, 1) &&
          Register(env, "libcore/io/Linux", linux_methods, 7) &&
